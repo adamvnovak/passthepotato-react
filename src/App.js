018863c659741ab1f7d5ca23ba5629631677b81c
adamvnovak <adamvnovak@protonmail.com>
2022-11-09 12:08:18 -0800

    pass the potato v1 finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
+import appstorebutton from './download-on-appstore.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Earth" > 
+          <h3 src={appstorebutton} className="App-logo" alt="logo">
+            🥔
+          </h3>
+          <h1 >
+            🌎
+          </h1>
+        </div>
+        {/* <img src={appstorebutton} className="App-logo" alt="logo" /> */}
+        <h2>
+          Pass The Potato
+        </h2>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://apple.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={appstorebutton} alt="appstorebutton" className='App-store-button' />
+        </a>
+      </header>
+      <header className="App-footer">
+        <a href="mailto:leaveylabs@gmail.com" target="_blank" rel="noopener noreferrer" >
+          Email us
+        </a>
+        <a href="/terms" target="_blank" rel="noopener noreferrer" >
+          Terms
+        </a>
+        <a href="/privacy" target="_blank" rel="noopener noreferrer" >
+          Privacy
         </a>
       </header>
     </div>
